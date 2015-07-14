6c41dee710b24addeecef3ee182e8de79ccc04e0
Guest User <student@epicodus.com>
2015-07-14 10:55:35 -0700

    Fix vote refreshing

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,20 @@ class Question < ActiveRecord::Base
   has_many :votes
   # has_many :comments
   # validates stuff
+
+  def votedOn(user_id, question)
+    vote = Vote.find_by(user_id: user_id, question_id: question.id)
+    if vote == nil
+      return false
+    else
+      if vote.option == 1
+        return question.option_1
+      elsif vote.option == 2
+        return question.option_2
+      else
+        return "There was an error processing your vote"
+      end
+    end
+  end
+
 end
